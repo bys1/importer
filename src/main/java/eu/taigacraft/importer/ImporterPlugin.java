fb66c4e90fc3427f6b1b70aeff1144086b6ad0ab
bys1 <bjorn.schonrock@live.nl>
2016-08-07 14:12:52 +0200

    Add way to register own importers

--- src/main/java/eu/taigacraft/importer/ImporterPlugin.java
+++ src/main/java/eu/taigacraft/importer/ImporterPlugin.java
@@ -1,21 +1,35 @@
 package eu.taigacraft.importer;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import eu.taigacraft.importer.permissions.*;
+import eu.taigacraft.importer.economy.*;
 
 public final class ImporterPlugin extends JavaPlugin {
 	
 	public final Logger logger = getLogger();
 	public final PluginDescriptionFile pdf = getDescription();
 	
+	public static final Map<String,PermissionsImporter> permissionsImporters = new HashMap<String,PermissionsImporter>();
+	public static final Map<String,EconomyImporter> economyImporters = new HashMap<String,EconomyImporter>();
+	
 	public final void onEnable() {
+		registerImporters();
 		logger.info(pdf.getName() + " v" + pdf.getVersion() + " has been enabled.");
 	}
 	
 	public final void onDisable() {
 		logger.info(pdf.getName() + " v" + pdf.getVersion() + " has been disabled.");
+	}
+	
+	private final void registerImporters() {
+		PermissionsImporter.register("PermissionsEx", new PermissionsExImporter());
+		PermissionsImporter.register("PermissionsBukkit", new PermissionsBukkitImporter());
 	}
 	
 	public static final ImporterPlugin getPlugin() {
