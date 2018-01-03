fc616042f7b7d67cfff016d994666707af22d964
bys1 <bjorn.schonrock@live.nl>
2018-01-03 10:47:18 +0100

    Update to new API
    Added a new API that works with Callbacks.

--- src/main/java/eu/taigacraft/importer/permissions/PermissionsImporter.java
+++ src/main/java/eu/taigacraft/importer/permissions/PermissionsImporter.java
@@ -3,55 +3,50 @@ package eu.taigacraft.importer.permissions;
 import java.util.List;
 
 import org.bukkit.OfflinePlayer;
+import org.bukkit.plugin.Plugin;
 
+import eu.taigacraft.importer.ImporterCallback;
 import eu.taigacraft.importer.ImporterPlugin;
 
 public interface PermissionsImporter {
 	
-	public static void register(String pluginName, PermissionsImporter importer) {
-		if (!(ImporterPlugin.permissionsImporters.containsKey(pluginName))) {
-			ImporterPlugin.permissionsImporters.put(pluginName,importer);
-			ImporterPlugin.getPlugin().logger.info("Registered PermissionsImporter from " + pluginName);
+	public static void register(final Plugin plugin, final PermissionsImporter importer) {
+		if (plugin == null) return;
+		if (importer == null) throw new IllegalArgumentException("Importer cannot be null");
+		if (!(ImporterPlugin.permissionsImporters.containsKey(plugin.getName()))) {
+			ImporterPlugin.permissionsImporters.put(plugin,importer);
+			ImporterPlugin.getPlugin().logger.info("Registered PermissionsImporter from " + plugin.getName());
 		}
 	}
 	
 	public static PermissionsImporter get() {
-		
-		for (String pluginName : ImporterPlugin.permissionsImporters.keySet()
-				.toArray(new String[ImporterPlugin.permissionsImporters.keySet().size()])) {
-			if (ImporterPlugin.getPlugin().getServer().getPluginManager().getPlugin(pluginName) != null) {
-				ImporterPlugin.getPlugin().logger.info("Hooking into " + pluginName);
-				return ImporterPlugin.permissionsImporters.get(pluginName);
+		for (Plugin plugin : ImporterPlugin.permissionsImporters.keySet()) {
+			if (plugin.isEnabled()) {
+				ImporterPlugin.getPlugin().logger.info("Hooking into " + plugin.getName());
+				return ImporterPlugin.permissionsImporters.get(plugin);
 			}
 		}
-		
 		return null;
 	}
 	
-	public String getRank(OfflinePlayer player);
+	public void getRank(OfflinePlayer player, ImporterCallback<String> callback);
 	
-	public List<String> getRanks(OfflinePlayer player);
+	public void getRanks(OfflinePlayer player, ImporterCallback<List<String>> callback);
 	
-	public String getPrefix(OfflinePlayer player);
+	public void getPrefix(OfflinePlayer player, ImporterCallback<String> callback);
 	
-	public String getPrefix(OfflinePlayer player, String worldname);
+	public void getPrefix(OfflinePlayer player, String worldname, ImporterCallback<String> callback);
 	
-	public String getPrefix(OfflinePlayer player, String worldname, String ladder);
+	public void getPrefix(OfflinePlayer player, String worldname, String ladder, ImporterCallback<String> callback);
 	
-	public String getSuffix(OfflinePlayer player);
+	public void getSuffix(OfflinePlayer player, ImporterCallback<String> callback);
 	
-	public String getSuffix(OfflinePlayer player, String worldname);
+	public void getSuffix(OfflinePlayer player, String worldname, ImporterCallback<String> callback);
 	
-	public String getSuffix(OfflinePlayer player, String worldname, String ladder);
+	public void getSuffix(OfflinePlayer player, String worldname, String ladder, ImporterCallback<String> callback);
 	
-	public Boolean hasPermission(OfflinePlayer player, String permission);
+	public void hasPermission(OfflinePlayer player, String permission, ImporterCallback<Boolean> callback);
 	
-	public Boolean hasPermission(OfflinePlayer player, String permission, String worldname);
-	
-	public void load(OfflinePlayer player);
-	
-	public void unload(OfflinePlayer player);
-	
-	public boolean isLoaded(OfflinePlayer player);
+	public void hasPermission(OfflinePlayer player, String permission, String worldname, ImporterCallback<Boolean> callback);
 	
 }
