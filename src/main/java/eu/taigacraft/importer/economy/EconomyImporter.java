7179a7ed6b1ed181b06b72769296e57f3a59efe5
bys1 <bjorn.schonrock@live.nl>
2018-01-03 10:45:51 +0100

    Update to new API

--- src/main/java/eu/taigacraft/importer/economy/EconomyImporter.java
+++ src/main/java/eu/taigacraft/importer/economy/EconomyImporter.java
@@ -1,31 +1,32 @@
 package eu.taigacraft.importer.economy;
 
 import org.bukkit.OfflinePlayer;
+import org.bukkit.plugin.Plugin;
 
+import eu.taigacraft.importer.ImporterCallback;
 import eu.taigacraft.importer.ImporterPlugin;
 
 public interface EconomyImporter {
 
-	public static void register(String pluginName, EconomyImporter importer) {
-		if (!(ImporterPlugin.economyImporters.containsKey(pluginName))) {
-			ImporterPlugin.economyImporters.put(pluginName,importer);
-			ImporterPlugin.getPlugin().logger.info("Registered EconomyImporter from " + pluginName);
+	public static void register(Plugin plugin, EconomyImporter importer) {
+		if (plugin == null) return;
+		if (importer == null) throw new IllegalArgumentException("Importer cannot be null");
+		if (!(ImporterPlugin.economyImporters.containsKey(plugin))) {
+			ImporterPlugin.economyImporters.put(plugin,importer);
+			ImporterPlugin.getPlugin().logger.info("Registered EconomyImporter from " + plugin.getName());
 		}
 	}
 	
 	public static EconomyImporter get() {
-		
-		for (String pluginName : ImporterPlugin.economyImporters.keySet()
-				.toArray(new String[ImporterPlugin.economyImporters.keySet().size()])) {
-			if (ImporterPlugin.getPlugin().getServer().getPluginManager().getPlugin(pluginName) != null) {
-				ImporterPlugin.getPlugin().logger.info("Hooking into " + pluginName);
-				return ImporterPlugin.economyImporters.get(pluginName);
+		for (Plugin plugin : ImporterPlugin.economyImporters.keySet()) {
+			if (plugin.isEnabled()) {
+				ImporterPlugin.getPlugin().logger.info("Hooking into " + plugin.getName());
+				return ImporterPlugin.economyImporters.get(plugin);
 			}
 		}
-		
 		return null;
 	}
 	
-	public double getBalance(OfflinePlayer player);
+	public void getBalance(OfflinePlayer player, ImporterCallback<Double> callback);
 	
 }
