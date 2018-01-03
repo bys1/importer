3c100111fd0d3abb614c4178f3e17a0244a2065c
bys1 <bjorn.schonrock@live.nl>
2018-01-03 10:56:54 +0100

    Update to new API

--- src/main/java/eu/taigacraft/importer/permissions/PermissionsBukkitImporter.java
+++ src/main/java/eu/taigacraft/importer/permissions/PermissionsBukkitImporter.java
@@ -1,73 +1,80 @@
 package eu.taigacraft.importer.permissions;
 
 import java.io.File;
+import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import eu.taigacraft.importer.FakePlayer;
+import eu.taigacraft.importer.ImporterCallback;
 
 public class PermissionsBukkitImporter implements PermissionsImporter {
 	
 	private FileConfiguration pb = YamlConfiguration.loadConfiguration(new File("plugins/PermissionsBukkit/config.yml"));
 	
-	public String getRank(OfflinePlayer player) {
-		return getRanks(player).get(0);
+	public void getRank(OfflinePlayer player, ImporterCallback<String> callback) {
+		callback.call(getRanks(player).get(0));
 	}
 	
-	public List<String> getRanks(OfflinePlayer player) {
+	public void getRanks(OfflinePlayer player, ImporterCallback<List<String>> callback) {
+		callback.call(getRanks(player));
+	}
+	
+	private final List<String> getRanks(OfflinePlayer player) {
 		return pb.getStringList("users." + player.getUniqueId().toString() + ".groups");
 	}
 	
 	@Deprecated
-	public String getPrefix(OfflinePlayer player) {
-		return null;
+	public void getPrefix(OfflinePlayer player, ImporterCallback<String> callback) {
+		callback.call(null);
 	}
 	
 	@Deprecated
-	public String getPrefix(OfflinePlayer player, String worldname) {
-		return null;
+	public void getPrefix(OfflinePlayer player, String worldname, ImporterCallback<String> callback) {
+		callback.call(null);
 	}
 	
 	@Deprecated
-	public String getPrefix(OfflinePlayer player, String worldname, String ladder) {
-		return null;
+	public void getPrefix(OfflinePlayer player, String worldname, String ladder, ImporterCallback<String> callback) {
+		callback.call(null);
 	}
 	
 	@Deprecated
-	public String getSuffix(OfflinePlayer player) {
-		return null;
+	public void getSuffix(OfflinePlayer player, ImporterCallback<String> callback) {
+		callback.call(null);
 	}
 	
 	@Deprecated
-	public String getSuffix(OfflinePlayer player, String worldname) {
-		return null;
+	public void getSuffix(OfflinePlayer player, String worldname, ImporterCallback<String> callback) {
+		callback.call(null);
 	}
 	
 	@Deprecated
-	public String getSuffix(OfflinePlayer player, String worldname, String ladder) {
-		return null;
+	public void getSuffix(OfflinePlayer player, String worldname, String ladder, ImporterCallback<String> callback) {
+		callback.call(null);
 	}
 	
-	public Boolean hasPermission(OfflinePlayer player, String permission) {
-		final String path = "groups." + getRank(player) + ".permissions." + permission;
-		return pb.get(path) != null ? pb.getBoolean(path) : false;
+	public void hasPermission(OfflinePlayer player, String permission, ImporterCallback<Boolean> callback) {
+		hasPermission(player, permission, null, callback);
 	}
 	
-	public Boolean hasPermission(OfflinePlayer player, String permission, String worldname) {
-		final String path = "groups." + getRank(player) + ".worlds." + worldname + ".permissions." + permission;
-		return pb.get(path) != null ? pb.getBoolean(path) : false;
+	public void hasPermission(OfflinePlayer player, String permission, String worldname, ImporterCallback<Boolean> callback) {
+		try {
+			final Method cpp = com.platymuus.bukkit.permissions.PermissionsPlugin.class.getDeclaredMethod("calculatePlayerPermissions",
+					Player.class, String.class);
+			@SuppressWarnings("unchecked")
+			final Map<String,Boolean> permissions = (Map<String,Boolean>) cpp.invoke(new FakePlayer(player), worldname);
+			if (permissions.containsKey(permission)) callback.call(permissions.get(permission));
+			else callback.call(false);
+		} catch (Exception e) {
+			e.printStackTrace();
+			callback.call(false);
+		}
 	}
 	
-	@Deprecated
-	public void load(OfflinePlayer player) {}
-	
-	@Deprecated
-	public void unload(OfflinePlayer player) {}
-	
-	@Deprecated
-	public boolean isLoaded(OfflinePlayer player) {
-		return false;
-	}
-
 }
