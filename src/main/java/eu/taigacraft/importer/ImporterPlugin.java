da9280700337dd6e8cc31c3e56238b7ada624488
bys1 <bjorn.schonrock@live.nl>
2018-01-03 10:44:14 +0100

    Update to v1.0

--- src/main/java/eu/taigacraft/importer/ImporterPlugin.java
+++ src/main/java/eu/taigacraft/importer/ImporterPlugin.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import eu.taigacraft.importer.permissions.*;
@@ -14,9 +16,10 @@ public final class ImporterPlugin extends JavaPlugin {
 	
 	public final Logger logger = getLogger();
 	public final PluginDescriptionFile pdf = getDescription();
+	private final PluginManager pm = getServer().getPluginManager();
 	
-	public static final Map<String,PermissionsImporter> permissionsImporters = new HashMap<String,PermissionsImporter>();
-	public static final Map<String,EconomyImporter> economyImporters = new HashMap<String,EconomyImporter>();
+	public static final Map<Plugin,PermissionsImporter> permissionsImporters = new HashMap<Plugin,PermissionsImporter>();
+	public static final Map<Plugin,EconomyImporter> economyImporters = new HashMap<Plugin,EconomyImporter>();
 	
 	public final void onEnable() {
 		registerImporters();
@@ -28,8 +31,15 @@ public final class ImporterPlugin extends JavaPlugin {
 	}
 	
 	private final void registerImporters() {
-		PermissionsImporter.register("PermissionsEx", new PermissionsExImporter());
-		PermissionsImporter.register("PermissionsBukkit", new PermissionsBukkitImporter());
+		PermissionsImporter.register(pm.getPlugin("PermissionsEx"), new PermissionsExImporter());
+		
+		// Null checks to prevent ClassNotFoundExceptions
+		if (pl("PermissionsBukkit")) PermissionsImporter.register(pm.getPlugin("PermissionsBukkit"), new PermissionsBukkitImporter());
+		if (pl("LuckPerms")) PermissionsImporter.register(pm.getPlugin("LuckPerms"), new LuckPermsImporter());
+	}
+	
+	private final boolean pl(final String name) {
+		return pm.getPlugin(name) != null;
 	}
 	
 	public static final ImporterPlugin getPlugin() {
