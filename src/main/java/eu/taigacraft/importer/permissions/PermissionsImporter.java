fbefd283191933fce9bf0a495498cbad3f658be8
bys1 <bjorn.schonrock@live.nl>
2018-07-05 12:24:08 +0000

    Added methods accepting Consumer<T>
    All methods will now also accept a Consumer<T> instead of an ImporterCallback<T> to allow lambda expressions and method references.

--- src/main/java/eu/taigacraft/importer/permissions/PermissionsImporter.java
+++ src/main/java/eu/taigacraft/importer/permissions/PermissionsImporter.java
@@ -1,12 +1,15 @@
 package eu.taigacraft.importer.permissions;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.Plugin;
 
 import eu.taigacraft.importer.ImporterCallback;
 import eu.taigacraft.importer.ImporterPlugin;
+
+import static eu.taigacraft.importer.ImporterCallback.fromConsumer;
 
 public interface PermissionsImporter {
 	
@@ -31,22 +34,62 @@ public interface PermissionsImporter {
 	
 	public void getRank(OfflinePlayer player, ImporterCallback<String> callback);
 	
+	public default void getRank(OfflinePlayer player, Consumer<String> consumer) {
+		getRank(player, fromConsumer(consumer, false));
+	}
+	
 	public void getRanks(OfflinePlayer player, ImporterCallback<List<String>> callback);
+	
+	public default void getRanks(OfflinePlayer player, Consumer<List<String>> consumer) {
+		getRanks(player, fromConsumer(consumer, false));
+	}
 	
 	public void getPrefix(OfflinePlayer player, ImporterCallback<String> callback);
 	
+	public default void getPrefix(OfflinePlayer player, Consumer<String> consumer) {
+		getPrefix(player, fromConsumer(consumer, false));
+	}
+	
 	public void getPrefix(OfflinePlayer player, String worldname, ImporterCallback<String> callback);
+	
+	public default void getPrefix(OfflinePlayer player, String worldname, Consumer<String> consumer) {
+		getPrefix(player, worldname, fromConsumer(consumer, false));
+	}
 	
 	public void getPrefix(OfflinePlayer player, String worldname, String ladder, ImporterCallback<String> callback);
 	
+	public default void getPrefix(OfflinePlayer player, String worldname, String ladder, Consumer<String> consumer) {
+		getPrefix(player, worldname, ladder, fromConsumer(consumer, false));
+	}
+	
 	public void getSuffix(OfflinePlayer player, ImporterCallback<String> callback);
+	
+	public default void getSuffix(OfflinePlayer player, Consumer<String> consumer) {
+		getSuffix(player, fromConsumer(consumer, false));
+	}
 	
 	public void getSuffix(OfflinePlayer player, String worldname, ImporterCallback<String> callback);
 	
+	public default void getSuffix(OfflinePlayer player, String worldname, Consumer<String> consumer) {
+		getSuffix(player, worldname, fromConsumer(consumer, false));
+	}
+	
 	public void getSuffix(OfflinePlayer player, String worldname, String ladder, ImporterCallback<String> callback);
+	
+	public default void getSuffix(OfflinePlayer player, String worldname, String ladder, Consumer<String> consumer) {
+		getSuffix(player, worldname, ladder, fromConsumer(consumer, false));
+	}
 	
 	public void hasPermission(OfflinePlayer player, String permission, ImporterCallback<Boolean> callback);
 	
+	public default void hasPermission(OfflinePlayer player, String permission, Consumer<Boolean> consumer) {
+		hasPermission(player, permission, fromConsumer(consumer, false));
+	}
+	
 	public void hasPermission(OfflinePlayer player, String permission, String worldname, ImporterCallback<Boolean> callback);
+	
+	public default void hasPermission(OfflinePlayer player, String permission, String worldname, Consumer<Boolean> consumer) {
+		hasPermission(player, permission, worldname, fromConsumer(consumer, false));
+	}
 	
 }
